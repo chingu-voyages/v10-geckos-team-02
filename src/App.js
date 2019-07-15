89ac6e5a0038a7770bb9bf272173414913c2f807
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-07-15 19:51:03 -0400

    feature/pick-a-pet-button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">PokeAPI</div>;
+  return (
+    <div className="App">
+      PokeAPI
+      <button>Pick A Pet</button>
+    </div>
+  );
 }
 
 export default App;
