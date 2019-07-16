ec4a35e2679c1537f02ba51e808efb1d3a2dd4b2
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-07-15 22:31:40 -0400

    feature/pick-a-pet-component

---
+++ src/components/Pet/Pet.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pet = () => (
+  <div>
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/717sXIvKw8L._SX425_.jpg"
+      alt="test"
+    />
+    <h2>Name</h2>
+  </div>
+);
+
+export default Pet;
---
+++ src/components/PickPet/PickPet.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./PickPet.css";
+
+import PetList from "../PetList/PetList";
+
+class PickPet extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      species: "Select your pet",
+      happiness: 10,
+      weight: ""
+    };
+  }
+
+  render() {
+    return (
+      <div className="pickpet-menu">
+        <h1>Pick A Pet</h1>
+        <h3>Click a pet below to start caring for it!</h3>
+        <PetList />
+      </div>
+    );
+  }
+}
+
+export default PickPet;
