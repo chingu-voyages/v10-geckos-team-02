074ab1508b3479da0afa4ab1b0a56314d6a2909a
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-07-17 20:18:57 -0400

    reformat App.js to handle state and deliver props to pages and components

--- src/pages/PickPet/PickPet.js
+++ src/pages/PickPet/PickPet.js
@@ -1,23 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import "./PickPet.css";
 
 import PetList from "../../components/PetList/PetList";
 
-class PickPet extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="pickpet-menu">
-        <h1>Pick A Pet</h1>
-        <h3>Click a pet below to start caring for it!</h3>
-        <PetList />
-      </div>
-    );
-  }
-}
+const PickPet = ({ petChosen, imageUrl, id, name }) => {
+  return (
+    <div className="pickpet-menu">
+      <h1>Pick A Pet</h1>
+      <h3>Click a pet below to start caring for it!</h3>
+      <PetList petChosen={petChosen} imageUrl={imageUrl} id={id} name={name} />
+    </div>
+  );
+};
 
 export default PickPet;
