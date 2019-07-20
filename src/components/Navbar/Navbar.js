849eb91d6e4dd4d41d9c8c00eb581dd6f2f5efda
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-07-20 19:18:46 -0400

    style Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,20 @@
 import React from "react";
+import logo from "../../img/logo.png";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>PokeAPI Navbar</div>;
+  return (
+    <div className="container">
+      <img className="logo" src={logo} alt="logo" />
+      <a className="title" href="/">
+        PokeAPI
+      </a>
+      <div className="routes">
+        <a href="/">Login</a>
+        <a href="/">Signup</a>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
