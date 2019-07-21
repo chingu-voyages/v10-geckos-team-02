0c464d2027fbd0ea9d662a5c24be18f891654c58
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-07-21 15:01:02 -0400

    integrate hamburger menu into navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "../../img/logo.png";
 import "./Navbar.css";
+import Menu from "react-burger-menu/lib/menus/slide";
+import styles from "./BurgerStyles.js";
 
-const Navbar = () => {
-  return (
-    <div className="container">
-      <div className="image-container">
-        <a href="/">
-          <img className="logo" src={logo} alt="logo" />
-        </a>
-      </div>
-      <div className="title-container">
-        <a className="title" href="/">
-          PokeAPI
-        </a>
-      </div>
-      <div className="route-container">
-        <div className="routes">
-          <a href="/">Login</a>
-          <a href="/">Signup</a>
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="image-container">
+          <a href="/">
+            <img className="logo" src={logo} alt="logo" />
+          </a>
+        </div>
+        <div className="title-container">
+          <a className="title" href="/">
+            PokeAPI
+          </a>
+        </div>
+        <div className="route-container">
+          <div className="routes">
+            {window.innerWidth <= 550 ? (
+              <div>
+                <Menu width={"100%"} styles={styles} right>
+                  <a href="/">Login</a>
+                  <a href="/">Signup</a>
+                </Menu>
+              </div>
+            ) : (
+              <div>
+                <a href="/">Login</a>
+                <a href="/">Signup</a>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Navbar;
