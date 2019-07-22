288a8c427764d676193327bb6f9a772dfa9fb2db
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-07-21 23:30:13 -0400

    refactor navbar as functional component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,41 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "../../img/logo.png";
 import "./Navbar.css";
 import Menu from "react-burger-menu/lib/menus/slide";
 import styles from "../../util/BurgerStyles.js";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="image-container">
-          <a href="/">
-            <img className="logo" src={logo} alt="logo" />
-          </a>
-        </div>
-        <div className="title-container">
-          <a className="title" href="/">
-            PokeAPI
-          </a>
-        </div>
-        <div className="route-container">
-          <div className="routes">
-            <div className="burger">
-              <Menu width={"100%"} styles={styles} right>
-                <a href="/">Login</a>
-                <a href="/">Signup</a>
-              </Menu>
-            </div>
-
-            <div className="no-burger">
+const Navbar = () => {
+  return (
+    <div className="container">
+      <div className="image-container">
+        <a href="/">
+          <img className="logo" src={logo} alt="logo" />
+        </a>
+      </div>
+      <div className="title-container">
+        <a className="title" href="/">
+          PokeAPI
+        </a>
+      </div>
+      <div className="route-container">
+        <div className="routes">
+          <div className="burger">
+            <Menu width={"100%"} styles={styles} right>
               <a href="/">Login</a>
               <a href="/">Signup</a>
-            </div>
+            </Menu>
+          </div>
+          <div className="no-burger">
+            <a href="/">Login</a>
+            <a href="/">Signup</a>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Navbar;
