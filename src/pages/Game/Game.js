98992885564a3790c7bab1d087a3c60045bfdf40
Thu Nguyen <ngthu1995@gmail.com>
2019-07-31 11:09:16 -0400

    make Game functional component

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -38,115 +38,41 @@ const images = [
   tempura
 ];
 
-class Game extends Component {
-  constructor() {
-    super();
-    this.state = {
-      cards: this.shuffleArray(images.slice()),
-      selected: [],
-      correct: []
-    };
-  }
-
-  onRestartClick = () => {
-    this.setState(
-      {
-        cards: this.shuffleArray(images.slice()),
-        selected: [],
-        correct: []
-      },
-      this.props.handleGameStatus(false)
-    );
-  };
-
-  onCardClick = clickedIndex => {
-    const { selected, cards, correct } = this.state;
-    if (selected.length === 0) {
-      // selecting a first card
-      this.setState({ selected: [clickedIndex] });
-    } else if (selected.length === 1) {
-      // they're selecting a second card
-      if (selected.length === 0) {
-        // selecting a first card
-        this.setState({ selected: [clickedIndex] });
-      } else if (selected.length === 1) {
-        // selecting a second card
-        if (cards[selected[0]] === cards[clickedIndex]) {
-          // If it's a match: add selected cards to `correct` and reset selection
-          this.setState({
-            correct: correct.concat([selected[0], clickedIndex]),
-            selected: []
-          });
-          if (correct.length === 0) {
-            this.props.handleGameOver();
-            this.props.handleGameStatus(true);
-
-            setTimeout(() => {
-              alert(`Congratulations! You won the game. Let's try again!`);
-
-              this.onRestartClick();
-            }, 1000);
-          }
-        } else {
-          // It's not a match
-          // Select it for now, and reset selection in a bit
-          // If it's not a match: Select it for now, and reset selection in a bit
-          this.setState({ selected: [selected[0], clickedIndex] });
-          setTimeout(() => {
-            this.setState({ selected: [] });
-          }, 1500);
-        }
-      }
-      // Otherwise they already have 2 selected and we don't want to do anything
-    }
-  };
-
-  shuffleArray = array => {
-    for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  };
-
-  render() {
-    const {
-      isTimeRunning,
-      credit,
-      happiness,
-      weight,
-      handleGameOver,
-      handleGameStatus
-    } = this.props;
-    console.log("I've been called");
-    return (
-      <div>
-        <div className="game-menu">
-          <h1 className="game-title">Matching Game</h1>
-          <h3>Earn credits by matching all the cards.</h3>
-          <div className="card__bars">
-            <Bar image={coin} metric={credit} />
-            <Bar image={heart} metric={happiness} />
-            <Bar image={scale} metric={weight} />
-            <Bar
-              image={timer}
-              metric={
-                <Timer
-                  isTimeRunning={isTimeRunning}
-                  handleGameOver={handleGameOver}
-                />
-              }
-            />
-          </div>
+const Game = ({
+  isTimeRunning,
+  credit,
+  happiness,
+  weight,
+  handleGameOver,
+  handleGameStatus
+}) => {
+  return (
+    <div>
+      <div className="game-menu">
+        <h1 className="game-title">Matching Game</h1>
+        <h3>Earn credits by matching all the cards.</h3>
+        <div className="card__bars">
+          <Bar image={coin} metric={credit} />
+          <Bar image={heart} metric={happiness} />
+          <Bar image={scale} metric={weight} />
+          <Bar
+            image={timer}
+            metric={
+              <Timer
+                isTimeRunning={isTimeRunning}
+                handleGameOver={handleGameOver}
+              />
+            }
+          />
         </div>
-
-        <MatchingGame
-          handleGameOver={handleGameOver}
-          handleGameStatus={handleGameStatus}
-        />
       </div>
-    );
-  }
-}
+
+      <MatchingGame
+        handleGameOver={handleGameOver}
+        handleGameStatus={handleGameStatus}
+      />
+    </div>
+  );
+};
 
 export default Game;
