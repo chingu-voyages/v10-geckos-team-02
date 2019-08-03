8c49d40a0bbc2111411021812e7f55ab65eba181
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-08-02 22:23:59 -0400

    reformat App.js to include footer links

--- src/App.js
+++ src/App.js
@@ -4,106 +4,21 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import ModalBox from "./components/ModalBox/ModalBox";
-// Pages
-import PetStore from "./pages/PetStore/PetStore";
-import Game from "./pages/Game/Game";
-import PickPet from "./pages/PickPet/PickPet";
-import Care from "./pages/Care/Care";
+import Body from "./components/Body/Body";
+//Pages
+import FAQ from "./pages/FAQ/FAQ";
+import Creators from "./pages/Creators/Creators";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      petChosen: false,
-      isPlaying: false,
-      isEating: false,
-      isTimeRunning: false,
-      isGameCompleted: false,
-      name: "testName",
-      species: "Animal",
-      happiness: 0,
-      weight: 0,
-      credit: 0,
-      image:
-        "https://i.pinimg.com/originals/28/ac/5a/28ac5a9ebf96eb7606cc8b9ca2c3abef.jpg"
+      isBodyOpen: true,
+      faqChosen: false,
+      creatorsChosen: false,
+      isGameCompleted: false
     };
   }
-
-  handleClick = (name, species, image, weight) => {
-    this.setState({ petChosen: true, name, species, image, weight });
-  };
-
-  onFeedClick = () => {
-    this.setState({ isEating: true });
-  };
-
-  onExerciseClick = () => {
-    this.setState({ isPlaying: true, isTimeRunning: true });
-  };
-
-  onCareBackClick = () => {
-    this.setState({ petChosen: false, happiness: 0, weight: 0, credit: 0 });
-  };
-
-  onStoreBackClick = () => {
-    this.setState({ isEating: false });
-  };
-
-  onGameBackClick = () => {
-    this.setState({ isPlaying: false });
-  };
-
-  handleFeed = () => {
-    if (this.state.credit < 3) return;
-    this.setState({
-      weight: this.state.weight + 5,
-      credit: this.state.credit - 3,
-      happiness: this.state.happiness + 2
-    });
-  };
-
-  handlePlay = () => {
-    if (this.state.weight < 3) return;
-    this.setState({
-      weight: this.state.weight - 3,
-      credit: this.state.credit + 4,
-      happiness: this.state.happiness + 3
-    });
-  };
-
-  handleGameStatus = gameCompleted => {
-    if (gameCompleted) {
-      this.setState({
-        credit: this.state.credit + 4,
-        happiness: this.state.happiness + 1,
-        isGameCompleted: true
-      });
-    } else {
-      this.setState({
-        isGameCompleted: false
-      });
-
-      if (this.state.happiness >= 1) {
-        this.setState({
-          happiness: this.state.happiness - 1
-        });
-      }
-    }
-  };
-
-  handleGameOver = () => {
-    this.setState({
-      isTimeRunning: false,
-      isPlaying: false
-    });
-  };
-
-  handleModalOpen = () => {
-    !this.state.isModalOpen &&
-      this.setState({
-        isModalOpen: true
-      });
-  };
 
   handleModalClose = () => {
     this.setState({
@@ -112,72 +27,52 @@ class App extends Component {
     });
   };
 
+  onFAQClick = () => {
+    this.setState({
+      isBodyOpen: false,
+      faqChosen: true,
+      creatorsChosen: false
+    });
+  };
+
+  onCreatorsClick = () => {
+    this.setState({
+      isBodyOpen: false,
+      faqChosen: false,
+      creatorsChosen: true
+    });
+  };
+
+  onBackClick = () => {
+    this.setState({
+      isBodyOpen: true,
+      faqChosen: false,
+      creatorsChosen: false
+    });
+  };
+
   render() {
     const {
-      petChosen,
-      isPlaying,
-      isEating,
-      name,
-      species,
-      happiness,
-      weight,
-      image,
-      credit,
-      isTimeRunning,
-      isGameCompleted
+      isGameCompleted,
+      isBodyOpen,
+      faqChosen,
+      creatorsChosen
     } = this.state;
 
     return (
       <div className="App">
         <Navbar />
-        {!petChosen ? (
-          <PickPet
-            name={name}
-            species={species}
-            weight={weight}
-            image={image}
-            handleClick={this.handleClick}
-          />
-        ) : !isEating && !isPlaying ? (
-          <Care
-            name={name}
-            species={species}
-            happiness={happiness}
-            weight={weight}
-            image={image}
-            credit={credit}
-            onPlay={this.handlePlay}
-            handleGameStatus={this.handleGameStatus}
-            onFeedClick={this.onFeedClick}
-            onExerciseClick={this.onExerciseClick}
-            onCareBackClick={this.onCareBackClick}
-          />
-        ) : isEating && !isPlaying ? (
-          <PetStore
-            happiness={happiness}
-            weight={weight}
-            credit={credit}
-            onFeed={this.handleFeed}
-            onStoreBackClick={this.onStoreBackClick}
-          />
+        {isBodyOpen ? (
+          <Body />
+        ) : faqChosen && !creatorsChosen ? (
+          <FAQ onBackClick={this.onBackClick} />
         ) : (
-          <div>
-            <Game
-              happiness={happiness}
-              weight={weight}
-              credit={credit}
-              isTimeRunning={isTimeRunning}
-              handleGameOver={this.handleGameOver}
-              handleGameStatus={isCompleted =>
-                this.handleGameStatus(isCompleted)
-              }
-              isGameCompleted={isGameCompleted}
-              handleModalOpen={this.handleModalOpen}
-              onGameBackClick={this.onGameBackClick}
-            />
-          </div>
+          <Creators onBackClick={this.onBackClick} />
         )}
-        <Footer />
+        <Footer
+          onFAQClick={this.onFAQClick}
+          onCreatorsClick={this.onCreatorsClick}
+        />
         {this.state.isModalOpen && (
           <ModalBox
             handleModalClose={this.handleModalClose}
--- src/pages/FAQ/FAQ.js
+++ src/pages/FAQ/FAQ.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./FAQ.css";
+// Images
+import back from "../../img/back.png";
 
-const FAQ = () => {
-  return <div>FAQ</div>;
+const FAQ = ({ onBackClick }) => {
+  return (
+    <div className="faq">
+      <h1 className="faq-title">Frequently Asked Questions</h1>
+      <img
+        src="http://noiselesschatter.com/wp-content/uploads/2016/07/thegreatmuppetcapergroup.png"
+        alt="muppet"
+      />
+      <img className="back" src={back} alt="back" onClick={onBackClick} />
+    </div>
+  );
 };
 
 export default FAQ;
