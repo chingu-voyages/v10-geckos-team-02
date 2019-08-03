b05e6df42367d7001e326c9608f177b8a9f31ee0
Marc <Marc S@Macintosh-2.local>
2019-08-02 20:29:22 -0400

    build pages for footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,16 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>Support</div>
-      <div>FAQ</div>
-      <div>Creators</div>
+      <a
+        href="https://github.com/chingu-voyages/v10-geckos-team-02"
+        className="footer-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        About
+      </a>
+      <a href="/faq">FAQ</a>
+      <a href="/creators">Creators</a>
     </footer>
   );
 };
