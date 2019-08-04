4304baf61e361e115d4cc1b71e6a173b6a7e02a9
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-08-04 19:24:36 -0400

    create card and text placement for Creators page

--- src/pages/Creators/Creators.js
+++ src/pages/Creators/Creators.js
@@ -7,10 +7,19 @@ const Creators = ({ onBackClick }) => {
   return (
     <div className="creators">
       <h1 className="creators-title">Creators</h1>
-      <img
-        src="https://www.dvdizzy.com/images/g/greatmuppetcaper-03.jpg"
-        alt="muppets"
-      />
+      <div className="creators-card">
+        <div className="creators-card-bg">
+          <p>
+            Cillum voluptate excepteur qui consectetur reprehenderit labore
+            quis. Adipisicing velit ea proident veniam sit elit labore sint
+            labore labore est minim. Cupidatat officia ipsum voluptate velit
+            anim voluptate. Laborum proident voluptate cillum duis aute officia
+            commodo eu excepteur esse ad enim culpa consequat. Non fugiat
+            laboris reprehenderit proident et adipisicing laborum cillum duis
+            dolore.
+          </p>
+        </div>
+      </div>
       <img className="back" src={back} alt="back" onClick={onBackClick} />
     </div>
   );
