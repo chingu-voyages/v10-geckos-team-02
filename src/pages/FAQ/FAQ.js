c1db3d931edd708b9bd61e6c882b24042a868575
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-08-06 18:01:14 -0400

    update FAQ

--- src/pages/FAQ/FAQ.js
+++ src/pages/FAQ/FAQ.js
@@ -10,6 +10,15 @@ const FAQ = ({ onBackClick }) => {
       <div className="faq-card">
         <div className="faq-card-bg">
           <div className="questions">
+            <h2>How does the app work?</h2>
+            <p>
+              Select a pet from the list of 8 kawaii kitties. You will then be
+              directed to the Care page where you will be able to play with the
+              cat or feed it with items from the Pet Store. You can keep track
+              of your credits, and the cat's happiness and weight values. You
+              can also gain credits by playing a special matching game. This
+              application is responsive, user-friendly and pet friendly.
+            </p>
             <h2>What’s your background, personally and professionally?</h2>
             <p>
               Marc lives in Montreal, Canada. He comes from a teaching
@@ -21,16 +30,6 @@ const FAQ = ({ onBackClick }) => {
               We had our first video meeting and agreed that a pet app would be
               fun to build. After realizing we both liked cats and Japanese
               food, NekoFudo was born!
-            </p>
-            <h2>What’s the biggest mistake you’ve made on a project?</h2>
-            <p>
-              Cillum pariatur elit fugiat non dolore veniam ut quis sit nostrud.
-              Fugiat duis irure nisi consectetur ea eu est adipisicing. Ullamco
-              est dolor id officia laboris ullamco aliqua labore culpa consequat
-              eiusmod in culpa voluptate. Nulla ex laborum sit est sit ut
-              officia. Voluptate ad commodo cupidatat reprehenderit consectetur
-              cupidatat. Nisi incididunt labore non id adipisicing tempor. Quis
-              in laboris velit et esse.
             </p>
             <h2>What’s the biggest achievement you’ve made on a project?</h2>
             <p>
@@ -50,7 +49,7 @@ const FAQ = ({ onBackClick }) => {
               eiusmod in culpa voluptate. Nulla ex laborum sit est sit ut
               officia. Voluptate ad commodo cupidatat reprehenderit consectetur
               cupidatat. Nisi incididunt labore non id adipisicing tempor. Quis
-              in laboris velit et esse.
+              in laboris velit et ess
             </p>
             <h2>
               How did you keep the project development moving according to
