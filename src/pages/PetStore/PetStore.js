0d8e18a5c82096ae3d2fdb325b6cd77ac11aa186
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-08-09 17:51:16 -0400

    format quotes with Prettier

--- src/pages/PetStore/PetStore.js
+++ src/pages/PetStore/PetStore.js
@@ -15,37 +15,37 @@ import back from "../../util/img/back.png";
 const PetStore = ({ credit, happiness, onFeed, weight, onStoreBackClick }) => {
   return (
     <div>
-      <div className="store-header">
-        <h1 className="store-title">Pet Store</h1>
+      <div className='store-header'>
+        <h1 className='store-title'>Pet Store</h1>
         <h3>Use credits to buy some delicious food for your pet.</h3>
       </div>
-      <div className="card">
-        <div className="card__bg">
-          <div className="store-card-bars">
+      <div className='card'>
+        <div className='card__bg'>
+          <div className='store-card-bars'>
             <Bar image={coin} metric={credit} />
             <Bar image={heart} metric={happiness} />
             <Bar image={scale} metric={weight} />
           </div>
-          <div className="food-items">
+          <div className='food-items'>
             <div className={credit >= 3 ? "food-item" : "food-item isolate"}>
-              <img className="item" alt="wool-ball" src={wool_ball} />
-              <h2 className="food-item-title">Wool ball</h2>
+              <img className='item' alt='wool-ball' src={wool_ball} />
+              <h2 className='food-item-title'>Wool ball</h2>
               <Feed onFeed={onFeed} credit={credit} />
             </div>
             <div className={credit >= 3 ? "food-item" : "food-item isolate"}>
-              <img className="item" alt="canned-food" src={canned_food} />
-              <h2 className="food-item-title">Canned food</h2>
+              <img className='item' alt='canned-food' src={canned_food} />
+              <h2 className='food-item-title'>Canned food</h2>
               <Feed onFeed={onFeed} credit={credit} />
             </div>
             <div className={credit >= 3 ? "food-item" : "food-item isolate"}>
-              <img className={"item"} alt="green-tea" src={green_tea} />
-              <h2 className="food-item-title">Green tea</h2>
+              <img className={"item"} alt='green-tea' src={green_tea} />
+              <h2 className='food-item-title'>Green tea</h2>
               <Feed onFeed={onFeed} credit={credit} />
             </div>
           </div>
         </div>
       </div>
-      <img className="back" src={back} alt="back" onClick={onStoreBackClick} />
+      <img className='back' src={back} alt='back' onClick={onStoreBackClick} />
     </div>
   );
 };
