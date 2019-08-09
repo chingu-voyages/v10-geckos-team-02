a5310b8410c68f2dc13aa598a15075c20cdfd08a
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-08-09 18:01:00 -0400

    Updates

---
+++ precache-manifest.f33b718e3baf95c1e212152f60bcca1f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bce62a53bc4166076207afd2ac78822",
+    "url": "/v10-geckos-team-02/index.html"
+  },
+  {
+    "revision": "4f9e95052846ca7e1410",
+    "url": "/v10-geckos-team-02/static/css/main.e1e4b42a.chunk.css"
+  },
+  {
+    "revision": "1a7a1d56bc246e90a538",
+    "url": "/v10-geckos-team-02/static/js/2.662fafee.chunk.js"
+  },
+  {
+    "revision": "4f9e95052846ca7e1410",
+    "url": "/v10-geckos-team-02/static/js/main.c7c5d7b1.chunk.js"
+  },
+  {
+    "revision": "7c7d806827b8a03777bb",
+    "url": "/v10-geckos-team-02/static/js/runtime~main.adee1597.js"
+  },
+  {
+    "revision": "9f3976585bbab55f646111bd8b4bc499",
+    "url": "/v10-geckos-team-02/static/media/Marc.9f397658.jpeg"
+  },
+  {
+    "revision": "db0276577ec27caf0abf81c67e9f26de",
+    "url": "/v10-geckos-team-02/static/media/Tina.db027657.jpeg"
+  },
+  {
+    "revision": "11c9ccedcf7ba9c86ea2a2f344387ef4",
+    "url": "/v10-geckos-team-02/static/media/bg_1.11c9cced.jpg"
+  },
+  {
+    "revision": "6318655a894cfaae6e50ac15c5ec5243",
+    "url": "/v10-geckos-team-02/static/media/canned_food.6318655a.png"
+  },
+  {
+    "revision": "66d81a520a3786b6cbbdcf5748269aaf",
+    "url": "/v10-geckos-team-02/static/media/maki.66d81a52.png"
+  },
+  {
+    "revision": "83596cd2be6d50633533fd21c9abd400",
+    "url": "/v10-geckos-team-02/static/media/ramen.83596cd2.png"
+  },
+  {
+    "revision": "19bf6dbe744c97c909b78dedbad0f581",
+    "url": "/v10-geckos-team-02/static/media/taiyaki.19bf6dbe.png"
+  },
+  {
+    "revision": "22b238becef2fafc5c3d4f43e3dba281",
+    "url": "/v10-geckos-team-02/static/media/temaki.22b238be.png"
+  },
+  {
+    "revision": "75800cb3da3a9e73ef2571345bb6339e",
+    "url": "/v10-geckos-team-02/static/media/tempura.75800cb3.png"
+  },
+  {
+    "revision": "bde7d3b064246da78457b7597f11af86",
+    "url": "/v10-geckos-team-02/static/media/timer.bde7d3b0.png"
+  },
+  {
+    "revision": "16e488d5a31ed04b6aedbb7c7d7488dc",
+    "url": "/v10-geckos-team-02/static/media/wool_ball.16e488d5.png"
+  }
+]);
